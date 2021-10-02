6fae371ee721f8f8cbd0b6243424d04c52a9a22d
ssook <cesces333@naver.com>
2021-10-02 23:15:02 +0900

    [feat]MemberController 생성, 계층 별 annotation 적용

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,17 +1,19 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-public class MemoryMemberRepository implements MemberRepository{
+@Repository
+public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
 
     @Override
     public Member save(Member member) {
         member.setId(++sequence);
-        store.put(member.getId(),member);
+        store.put(member.getId(), member);
         return member;
     }
 
